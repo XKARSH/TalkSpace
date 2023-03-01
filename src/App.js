eed3ba81010f011b7f755d4d7c0f10cb363dd31a
UtKaRsh7K <utkarshk2207@gmail.com>
2023-03-01 19:17:52 +0530

    Completed app functionality, Styling is pending

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "firebase/compat/auth";
+import React, { useEffect, useState } from "react";
+import Button from "./components/Button";
+import Channel from "./components/Channel";
+//Firebase Dependencies
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_APP_ID,
+});
+
+const auth = firebase.auth();
+const db = firebase.firestore();
 
 function App() {
+  const [user, setUser] = useState(() => auth.currentUser);
+  const [initializing, setInitializing] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+      if (initializing) {
+        setInitializing(false);
+      }
+    });
+
+    //Cleanup subscription
+    return unsubscribe;
+  }, [initializing]);
+
+  const signInWithGoogle = async () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.useDeviceLanguage();
+
+    try {
+      await firebase.auth().signInWithPopup(provider);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const signOut = async () => {
+    try {
+      await firebase.auth().signOut();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  if (initializing) return "Loading...";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user ? (
+        <>
+          <Button onClick={signOut}>Sign Out</Button>
+          <Channel user={user} db={db} />
+        </>
+      ) : (
+        <Button onClick={signInWithGoogle}>Sign in with Google</Button>
+      )}
     </div>
   );
 }
-
 export default App;
